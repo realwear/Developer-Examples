804c529411f87249cedf2418b2ca83f5626f4b2e
Kieren Searle <kieren.searle@realwear.com>
2017-10-17 15:21:35 -0400

    Fix references to old package name (Fixes bug HMT-200)
    Fixed a couple of references that were left behind when the package name was updated

--- Developer-Examples/hmt1developerexamples/src/androidTest/java/com/realwear/hmt1developerexamples/ExampleInstrumentedTest.java
+++ Developer-Examples/hmt1developerexamples/src/androidTest/java/com/realwear/hmt1developerexamples/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.realwear.hmt1developertest;
+package com.realwear.hmt1developerexamples;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.realwear.hmt1developertest", appContext.getPackageName());
+        assertEquals("com.realwear.hmt1developerexamples", appContext.getPackageName());
     }
 }
