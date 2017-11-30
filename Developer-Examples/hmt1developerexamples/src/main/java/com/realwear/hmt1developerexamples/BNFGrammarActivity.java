bcf3e8257e6aa5732dee3108660ee4d1f7da4535
jackblitz <lukehopkinsdev@gmail.com>
2017-11-30 08:39:31 -0800

    BNF grammar now use's broadcast reciever
    BNF grammar is now sent via intent and recieved using broadcast reciever

--- Developer-Examples/hmt1developerexamples/src/main/java/com/realwear/hmt1developerexamples/BNFGrammarActivity.java
+++ Developer-Examples/hmt1developerexamples/src/main/java/com/realwear/hmt1developerexamples/BNFGrammarActivity.java
@@ -17,6 +17,21 @@ import android.widget.Toast;
 public class BNFGrammarActivity extends Activity {
 
     private static final String ACTION_SPEECH_EVENT = "com.realwear.wearhf.intent.action.SPEECH_EVENT";
+    //
+    // Intent actions for registering voice commands and for being notified when they are triggered
+    //
+    private static final String ACTION_OVERRIDE_COMMANDS =
+            "com.realwear.wearhf.intent.action.OVERRIDE_COMMANDS";
+    private static final String ACTION_RESTORE_COMMANDS =
+            "com.realwear.wearhf.intent.action.RESTORE_COMMANDS";
+
+    // Identifier for the package that is setting the voice commands
+    private static final String EXTRA_SOURCE_PACKAGE =
+            "com.realwear.wearhf.intent.extra.SOURCE_PACKAGE";
+
+    // Identifier used when registering voice commands
+    private static final String EXTRA_COMMANDS =
+            "com.realwear.wearhf.intent.extra.COMMANDS";
 
     /*
         BNF Grammar for allowing a user to say the hour and minute
@@ -42,8 +57,6 @@ public class BNFGrammarActivity extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.bnf_acitivity);
 
-        View bnfLayout = findViewById(R.id.bnf_view);
-        bnfLayout.setContentDescription("hf_override:" + timeBNF);
     }
 
     @Override
@@ -51,6 +64,10 @@ public class BNFGrammarActivity extends Activity {
         super.onPause();
 
         if (asrBroadcastReceiver!=null)unregisterReceiver(asrBroadcastReceiver);
+
+        Intent intent = new Intent(ACTION_RESTORE_COMMANDS);
+        intent.putExtra(EXTRA_SOURCE_PACKAGE, getPackageName());
+        sendBroadcast(intent);
     }
 
     @Override
@@ -58,6 +75,8 @@ public class BNFGrammarActivity extends Activity {
         super.onResume();
 
         registerReceiver(asrBroadcastReceiver, new IntentFilter(ACTION_SPEECH_EVENT));
+
+        sendCommands();
     }
 
     private BroadcastReceiver asrBroadcastReceiver = new BroadcastReceiver() {
@@ -69,7 +88,19 @@ public class BNFGrammarActivity extends Activity {
                 String asrCommand = intent.getStringExtra("command");
                 Toast.makeText(getBaseContext(), asrCommand, Toast.LENGTH_LONG).show();
             }
+
+            //Always reload commands.
+            sendCommands();
         }
     };
 
+    /**
+     * Register voice commands
+     */
+    private void sendCommands() {
+        Intent intent = new Intent(ACTION_OVERRIDE_COMMANDS);
+        intent.putExtra(EXTRA_SOURCE_PACKAGE, getPackageName());
+        intent.putExtra(EXTRA_COMMANDS, timeBNF);
+        sendBroadcast(intent);
+    }
 }
