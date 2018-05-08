a5f88a7d11746a7e86b347fe5bcdffeded4fe126
Chris Parkinson <chris@realwear.com>
2018-05-08 11:19:43 -0700

    Moved BNF Grammar into XML Layout File (workaround for BNF bug in 8.09)

--- hmt1developerexamples/src/main/java/com/realwear/hmt1developerexamples/BNFGrammarActivity.java
+++ hmt1developerexamples/src/main/java/com/realwear/hmt1developerexamples/BNFGrammarActivity.java
@@ -1,10 +1,3 @@
-/**
- * RealWear Development Software, Source Code and Object Code
- * (c) RealWear, Inc. All rights reserved.
- * <p>
- * Contact info@realwear.com for further information about the use of this code.
- */
-
 package com.realwear.hmt1developerexamples;
 
 import android.app.Activity;
@@ -13,16 +6,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.Toast;
 
 /**
- * Activity that shows how to register BNF grammar on a HMT-1 device
+ * Created by Luke on 29/11/2017.
  */
-public class BNFGrammarActivity extends Activity {
-    private static final String ACTION_SPEECH_EVENT =
-            "com.realwear.wearhf.intent.action.SPEECH_EVENT";
 
+public class BNFGrammarActivity extends Activity {
+
+    private static final String ACTION_SPEECH_EVENT = "com.realwear.wearhf.intent.action.SPEECH_EVENT";
     //
     // Intent actions for registering voice commands and for being notified when they are triggered
     //
@@ -36,9 +30,12 @@ public class BNFGrammarActivity extends Activity {
             "com.realwear.wearhf.intent.extra.SOURCE_PACKAGE";
 
     // Identifier used when registering voice commands
-    private static final String EXTRA_COMMANDS = "com.realwear.wearhf.intent.extra.COMMANDS";
+    private static final String EXTRA_COMMANDS =
+            "com.realwear.wearhf.intent.extra.COMMANDS";
 
-    // BNF Grammar for allowing a user to say the hour and minute
+    /*
+        BNF Grammar for allowing a user to say the hour and minute
+     */
     private static String timeBNF = "#BNF+EM V2.0;" +
             "!grammar Commands;\n" +
             "!start <Commands>;\n" +
@@ -58,16 +55,15 @@ public class BNFGrammarActivity extends Activity {
         getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.bnf_acitivity);
+        setContentView(R.layout.bnf_activity);
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        if (asrBroadcastReceiver != null) {
-            unregisterReceiver(asrBroadcastReceiver);
-        }
+        if (asrBroadcastReceiver!=null)unregisterReceiver(asrBroadcastReceiver);
 
         Intent intent = new Intent(ACTION_RESTORE_COMMANDS);
         intent.putExtra(EXTRA_SOURCE_PACKAGE, getPackageName());
@@ -79,21 +75,21 @@ public class BNFGrammarActivity extends Activity {
         super.onResume();
 
         registerReceiver(asrBroadcastReceiver, new IntentFilter(ACTION_SPEECH_EVENT));
+
         sendCommands();
     }
 
-    /**
-     * Broadcast receiver for being notified when voice commands are triggered
-     */
     private BroadcastReceiver asrBroadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
+                 Log.d("Test:", "Broadcast Action=" + action);
             if (action.equals(ACTION_SPEECH_EVENT)) {
                 String asrCommand = intent.getStringExtra("command");
                 Toast.makeText(getBaseContext(), asrCommand, Toast.LENGTH_LONG).show();
             }
 
+            //Always reload commands.
             sendCommands();
         }
     };
@@ -102,9 +98,9 @@ public class BNFGrammarActivity extends Activity {
      * Register voice commands
      */
     private void sendCommands() {
-        Intent intent = new Intent(ACTION_OVERRIDE_COMMANDS);
-        intent.putExtra(EXTRA_SOURCE_PACKAGE, getPackageName());
-        intent.putExtra(EXTRA_COMMANDS, timeBNF);
-        sendBroadcast(intent);
+//        Intent intent = new Intent(ACTION_OVERRIDE_COMMANDS);
+//        intent.putExtra(EXTRA_SOURCE_PACKAGE, getPackageName());
+//        intent.putExtra(EXTRA_COMMANDS, timeBNF);
+//        sendBroadcast(intent);
     }
 }
