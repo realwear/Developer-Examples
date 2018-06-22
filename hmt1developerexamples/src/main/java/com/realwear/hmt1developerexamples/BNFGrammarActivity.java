e2fdca9b67509b9dd5d2174606deeec1c8d32bde
Kieren Searle <kieren.searle@realwear.com>
2018-06-22 12:44:08 -0400

    Fix BNF Example (Bug HMT-1075)
    Fixed the BNF example so that it now works and the code follow RW conventions

--- hmt1developerexamples/src/main/java/com/realwear/hmt1developerexamples/BNFGrammarActivity.java
+++ hmt1developerexamples/src/main/java/com/realwear/hmt1developerexamples/BNFGrammarActivity.java
@@ -1,3 +1,10 @@
+/**
+ * RealWear Development Software, Source Code and Object Code
+ * (c) RealWear, Inc. All rights reserved.
+ * <p>
+ * Contact info@realwear.com for further information about the use of this code.
+ */
+
 package com.realwear.hmt1developerexamples;
 
 import android.app.Activity;
@@ -11,32 +18,34 @@ import android.view.WindowManager;
 import android.widget.Toast;
 
 /**
- * Created by Luke on 29/11/2017.
+ * Activity that shows how to register BNF grammar on a HMT-1 device
  */
-
 public class BNFGrammarActivity extends Activity {
+    // Identifier for speech events
+    private static final String ACTION_SPEECH_EVENT =
+            "com.realwear.wearhf.intent.action.SPEECH_EVENT";
 
-    private static final String ACTION_SPEECH_EVENT = "com.realwear.wearhf.intent.action.SPEECH_EVENT";
-    //
-    // Intent actions for registering voice commands and for being notified when they are triggered
-    //
-      private static final String ACTION_RESTORE_COMMANDS =
-            "com.realwear.wearhf.intent.action.RESTORE_COMMANDS";
-
-    // Identifier for the package that is setting the voice commands
-    private static final String EXTRA_SOURCE_PACKAGE =
-            "com.realwear.wearhf.intent.extra.SOURCE_PACKAGE";
-
-    // Identifier used when registering voice commands
+    // Intent actions for taking over voice commands
+    private static final String ACTION_OVERRIDE_COMMANDS =
+            "com.realwear.wearhf.intent.action.OVERRIDE_COMMANDS";
     private static final String EXTRA_COMMANDS =
             "com.realwear.wearhf.intent.extra.COMMANDS";
 
+    // Intent actions for restoring voice commands when we no longer want to capture
+    private static final String ACTION_RESTORE_COMMANDS =
+            "com.realwear.wearhf.intent.action.RESTORE_COMMANDS";
 
-    /**
-     * Called when the activity is created
-     *
-     * @param savedInstanceState See Android docs
-     */
+    // Identifier for the package
+    private static final String EXTRA_SOURCE_PACKAGE =
+            "com.realwear.wearhf.intent.extra.SOURCE_PACKAGE";
+
+    private final String TIME_BNF = "#BNF+EM V2.0;" +
+            "!grammar Commands;\n" +
+            "!start <Commands>;\n" +
+            "<Commands>:<global_commands>|<Hour> !optional(<Minute>);\n" +
+            "<Minute>:1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59;\n" +
+            "<Hour>:1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24;";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,14 +54,15 @@ public class BNFGrammarActivity extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.bnf_activity);
-
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        if (asrBroadcastReceiver!=null)unregisterReceiver(asrBroadcastReceiver);
+        if (asrBroadcastReceiver != null) {
+            unregisterReceiver(asrBroadcastReceiver);
+        }
 
         Intent intent = new Intent(ACTION_RESTORE_COMMANDS);
         intent.putExtra(EXTRA_SOURCE_PACKAGE, getPackageName());
@@ -68,13 +78,16 @@ public class BNFGrammarActivity extends Activity {
         sendCommands();
     }
 
+    /**
+     * Broadcast receiver for being notified when speech events occur
+     */
     private BroadcastReceiver asrBroadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
-                 Log.d("Test:", "Broadcast Action=" + action);
             if (action.equals(ACTION_SPEECH_EVENT)) {
                 String asrCommand = intent.getStringExtra("command");
+                Log.d("asrCommand", "onReceive: " + asrCommand);
                 Toast.makeText(getBaseContext(), asrCommand, Toast.LENGTH_LONG).show();
             }
 
@@ -87,9 +100,9 @@ public class BNFGrammarActivity extends Activity {
      * Register voice commands
      */
     private void sendCommands() {
-//        Intent intent = new Intent(ACTION_OVERRIDE_COMMANDS);
-//        intent.putExtra(EXTRA_SOURCE_PACKAGE, getPackageName());
-//        intent.putExtra(EXTRA_COMMANDS, timeBNF);
-//        sendBroadcast(intent);
+        Intent intent = new Intent(ACTION_OVERRIDE_COMMANDS);
+        intent.putExtra(EXTRA_SOURCE_PACKAGE, getPackageName());
+        intent.putExtra(EXTRA_COMMANDS, TIME_BNF);
+        sendBroadcast(intent);
     }
 }
