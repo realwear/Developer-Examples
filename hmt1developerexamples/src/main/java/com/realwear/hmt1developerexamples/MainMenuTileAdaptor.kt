63f9ac500503fa540cc43508ddd1279edd2d68e0
Alex Mason <alex.mason@realwear.com>
2022-03-30 09:37:15 -0700

    Update MainMenuTileAdaptor
    - Convert to Kotlin

--- hmt1developerexamples/src/main/java/com/realwear/hmt1developerexamples/MainMenuTileAdaptor.kt
+++ hmt1developerexamples/src/main/java/com/realwear/hmt1developerexamples/MainMenuTileAdaptor.kt
@@ -1,45 +1,33 @@
 /**
  * RealWear Development Software, Source Code and Object Code
  * (c) RealWear, Inc. All rights reserved.
- * <p>
+ *
+ *
  * Contact info@realwear.com for further information about the use of this code.
  */
+package com.realwear.hmt1developerexamples
 
-package com.realwear.hmt1developerexamples;
-
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.view.View
+import android.widget.BaseAdapter
+import android.view.ViewGroup
 
 /**
  * Adapter used to drive the tile view
  */
-public class MainMenuTileAdaptor extends BaseAdapter {
-    private View mTileList[];
-
-    /**
-     * Constructor
-     *
-     * @param tileList The tile list to display
-     */
-    public MainMenuTileAdaptor(View[] tileList) {
-        mTileList = tileList;
-    }
-
+class MainMenuTileAdaptor
+/**
+ * Constructor
+ *
+ * @param _tileList The tile list to display
+ */(private val _tileList: Array<View>) : BaseAdapter() {
     /**
      * Get the number of tiles in the adaptor
      *
      * @return The number of tiles
      */
-    @Override
-    public int getCount() {
-        if (mTileList == null) {
-            return 0;
-        }
-
-        return mTileList.length;
+    override fun getCount(): Int {
+        return _tileList.size
     }
-
 
     /**
      * Get a tile from the data set
@@ -47,13 +35,8 @@ public class MainMenuTileAdaptor extends BaseAdapter {
      * @param position The index of the tile to get
      * @return The tile or null if not found
      */
-    @Override
-    public Object getItem(int position) {
-        if (mTileList == null || position < 0 || position >= mTileList.length) {
-            return null;
-        }
-
-        return mTileList[position];
+    override fun getItem(position: Int): Any {
+        return _tileList[position]
     }
 
     /**
@@ -62,9 +45,8 @@ public class MainMenuTileAdaptor extends BaseAdapter {
      * @param position The index of the tile to get the item ID for
      * @return Always returns position
      */
-    @Override
-    public long getItemId(int position) {
-        return position;
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
     }
 
     /**
@@ -75,9 +57,8 @@ public class MainMenuTileAdaptor extends BaseAdapter {
      * @param parent      The parent that this view will eventually be attached to
      * @return The tile view
      */
-    @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        return mTileList[position];
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        return _tileList[position]
     }
 
     /**
@@ -86,7 +67,7 @@ public class MainMenuTileAdaptor extends BaseAdapter {
      * @param position The index of the tile
      * @return Always true
      */
-    public boolean isEnabled(int position) {
-        return true;
+    override fun isEnabled(position: Int): Boolean {
+        return true
     }
 }
