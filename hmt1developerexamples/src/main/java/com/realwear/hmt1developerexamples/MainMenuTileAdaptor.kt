635857790606f704629b0c1df8df65d89f4c1692
Alex Mason <alex.mason@realwear.com>
2022-04-04 11:55:31 -0700

    Update MainMenuTileAdaptor.kt

--- hmt1developerexamples/src/main/java/com/realwear/hmt1developerexamples/MainMenuTileAdaptor.kt
+++ hmt1developerexamples/src/main/java/com/realwear/hmt1developerexamples/MainMenuTileAdaptor.kt
@@ -12,60 +12,40 @@ import android.widget.BaseAdapter
 import android.view.ViewGroup
 
 /**
- * Adapter used to drive the tile view
+ * Adapter used to drive the tile view with the [tileList] passed in.
  */
-class MainMenuTileAdaptor
-/**
- * Constructor
- *
- * @param _tileList The tile list to display
- */(private val _tileList: Array<View>) : BaseAdapter() {
+class MainMenuTileAdaptor(private val tileList: Array<View>) : BaseAdapter() {
     /**
      * Get the number of tiles in the adaptor
-     *
-     * @return The number of tiles
      */
     override fun getCount(): Int {
-        return _tileList.size
+        return tileList.size
     }
 
     /**
-     * Get a tile from the data set
-     *
-     * @param position The index of the tile to get
-     * @return The tile or null if not found
+     * Get a tile from the tile list at [position].
      */
     override fun getItem(position: Int): Any {
-        return _tileList[position]
+        return tileList[position]
     }
 
     /**
-     * Get the item ID for a tile. Always returns position.
-     *
-     * @param position The index of the tile to get the item ID for
-     * @return Always returns position
+     * Get the item ID for a tile. Always returns [position].
      */
     override fun getItemId(position: Int): Long {
         return position.toLong()
     }
 
     /**
-     * Get the tile view at the specified position in the data set
-     *
-     * @param position    The index of the tile view to get
-     * @param convertView The old view to reuse, if possible
-     * @param parent      The parent that this view will eventually be attached to
-     * @return The tile view
+     * Get the tile view at the specified [position] in the data set, passing in a [convertView] to
+     * reuse if possible and the [parent] ViewGroup to be attached to.
      */
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return _tileList[position]
+        return tileList[position]
     }
 
     /**
-     * Get if a tile is clickable
-     *
-     * @param position The index of the tile
-     * @return Always true
+     * Get if a tile is clickable at [position]. Always return true.
      */
     override fun isEnabled(position: Int): Boolean {
         return true
